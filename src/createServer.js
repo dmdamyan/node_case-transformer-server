0960eb83551e234b7f6362da73227dded6d9603b
Dmitrii Damyan <dmitriignc@gmail.com>
2026-03-30 19:40:24 +0300

    add solution

--- src/createServer.js
+++ src/createServer.js
@@ -32,23 +32,23 @@ function createServer() {
     if (!text) {
       errors.push({
         message:
-          'Text to convert is required.' +
-          ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+          // eslint-disable-next-line max-len
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     }
 
     if (!toCase) {
       errors.push({
         message:
-          '"toCase" query param is required.' +
-          ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+          // eslint-disable-next-line max-len
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     } else {
       if (!validCaseValues.includes(toCase)) {
         errors.push({
           message:
-            'This case is not supported.' +
-            ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+            // eslint-disable-next-line max-len
+            'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
         });
       }
     }
